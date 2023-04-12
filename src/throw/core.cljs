1d30d6c5487551cc289fbe618366a5486e00b9d7
katja lusma <katja.lusma@siili.com>
2023-04-12 11:36:15 +0300

    Add placeholders for dice - plus new ideas

--- src/throw/core.cljs
+++ src/throw/core.cljs
@@ -21,6 +21,7 @@
 (defn die? [maybe-die]
   (boolean (re-matches #"^-?\d{1,3}d\d{1,3}" maybe-die)))
 
+;; in case of a special case, one can (BUT SHOULD HIM?) also give one die with "d6" and NOT "1d6"
 (defn parse-input [dice]
   (let [dice-split (str/split dice #"")
         final-state (reduce
@@ -37,21 +38,35 @@
 (defn calculate [throw]
   (reduce + (map int throw)))
 
-(defn count-dice [input-dice]
+(defn throw-dice [input-dice]
   (map #(throw-one-die %) input-dice))
+
+;; input-parsed: map over this, with a fn that deducts which fn to use for it AND returns an anonym fn with no params, 
+;; then this all return vec of fns 
+;; fex [throw fn-anynym-no-params throw ]
 
 (defn parse-and-swap [input]
   (let [input-parsed (parse-input input) ;; this could be a ->>
-        dice-thrown (vec (count-dice (filterv die? input-parsed))) ;; this is stupid.
+        dice-thrown (vec (throw-dice (filterv die? input-parsed))) ;; this is stupid.
         just-numbers (map int (filterv #(not= die? %) input-parsed)) ;; this is also stupid.
         throw-parsed (concat dice-thrown just-numbers)
-        throw-calculated (calculate throw-parsed)] 
+        throw-calculated (calculate throw-parsed)]
     (reset! result throw-calculated)))
+
+(def throwable-dice-sides [4 6 8 10 12 20 100])
+
+(defn die-button [d]
+  [:div {:class "die-img"} d])
+
+(defn dice []
+  [:div {:class "die-container"} 
+   (map #(die-button %) throwable-dice-sides)])
 
 (defn home-page []
   (fn []
     [:span.main
      [:h1 "Throw!"]
+     [dice]
      [:div
       [:input {:type :text
                :value @input
